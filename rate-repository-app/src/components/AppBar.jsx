773624211c885e6325ab8f08a840b93e245c976a
myuwi <myuwi@proton.me>
2024-11-12 13:42:25 +0200

    complete 10.6

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,12 +1,14 @@
-import { View, StyleSheet, Pressable } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
 import Text from "./Text";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
+    gap: theme.spacing.lg,
     paddingTop: Constants.statusBarHeight,
     paddingHorizontal: 16,
     height: 64 + Constants.statusBarHeight,
@@ -14,20 +16,21 @@ const styles = StyleSheet.create({
   },
 });
 
-const AppBarTab = ({ children }) => {
+const AppBarTab = ({ to, children }) => {
   return (
-    <Pressable>
+    <Link to={to}>
       <Text color="onPrimary" weight="bold" size="medium">
         {children}
       </Text>
-    </Pressable>
+    </Link>
   );
 };
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab>Repositories</AppBarTab>
+      <AppBarTab to="/">Repositories</AppBarTab>
+      <AppBarTab to="/signin">Sign in</AppBarTab>
     </View>
   );
 };
